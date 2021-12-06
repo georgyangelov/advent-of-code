27ba34fafc32f5c235c8870b30018c86da2307f6
Georgy Angelov <georgyangelov@gmail.com>
2021-12-06 07:22:43 +0200

    Add day 6 code

--- 2021/6/part_one.rb
+++ 2021/6/part_one.rb
@@ -2,24 +2,34 @@ module PartOne
   extend self
 
   def example_solution
+    5934
   end
 
   def actual_solution
+    353079
   end
 
   def main(input)
-    parsed_input = input.split("\n").map do |line|
-      line
-    end
+    parsed_input = input.split(",").map(&:to_i)
 
     solve parsed_input
   end
 
-  def solve(lines)
+  def solve(fish)
+    80.times { fish = process_day(fish) }
 
+    fish.size
   end
 
   private
 
-
+  def process_day(fish)
+    fish = fish.flat_map do |f|
+      if f == 0
+        [6, 8]
+      else
+        [f - 1]
+      end
+    end
+  end
 end
--- 2021/6/part_two.rb
+++ 2021/6/part_two.rb
@@ -2,24 +2,34 @@ module PartTwo
   extend self
 
   def example_solution
+    26984457539
   end
 
   def actual_solution
+    1605400130036
   end
 
   def main(input)
-    parsed_input = input.split("\n").map do |line|
-      line
-    end
+    parsed_input = input.split(',').map(&:to_i)
 
     solve parsed_input
   end
 
-  def solve(lines)
+  def solve(fish)
+    fish = 9.times.map { |i| fish.count { |f| f == i } }
+    256.times { fish = process_day(fish) }
 
+    fish.sum
   end
 
   private
 
+  def process_day(fish)
+    first = fish.shift
 
+    fish.push(first)
+    fish[6] += first
+
+    fish
+  end
 end
