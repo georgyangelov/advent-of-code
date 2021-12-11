ad21357196a15987b263f1540df9d5b659a1be4d
Georgy Angelov <georgyangelov@gmail.com>
2021-12-11 10:49:45 +0200

    Day 11

---
+++ 2021/11/part_two.rb
@@ -0,0 +1,88 @@
+module PartTwo
+  extend self
+
+  def example_solution
+    195
+  end
+
+  def actual_solution
+    276
+  end
+
+  def main(input)
+    parsed_input = input.split("\n").map do |line|
+      line.chars.map(&:to_i)
+    end
+
+    solve parsed_input
+  end
+
+  def solve(matrix)
+    step = 0
+
+    loop do
+      flashes = simulate_step(matrix)
+      step += 1
+
+      return step if flashes == matrix.size * matrix.first.size
+    end
+  end
+
+  private
+
+  def simulate_step(matrix)
+    each_cell matrix do |row, col|
+      matrix[row][col] += 1
+    end
+
+    flashes = 0
+
+    loop do
+      octopus_flashed = false
+
+      each_cell matrix do |row, col|
+        next unless matrix[row][col] && matrix[row][col] > 9
+
+        octopus_flashed = true
+        flashes += 1
+        matrix[row][col] = nil
+
+        each_adjacent_cell matrix, row, col do |adjacent_row, adjacent_col|
+          unless matrix[adjacent_row][adjacent_col].nil?
+            matrix[adjacent_row][adjacent_col] += 1
+          end
+        end
+      end
+
+      break unless octopus_flashed
+    end
+
+    each_cell matrix do |row, col|
+      matrix[row][col] = 0 if matrix[row][col].nil?
+    end
+
+    flashes
+  end
+
+  def each_cell(matrix)
+    matrix.each_index do |row|
+      matrix[row].each_index do |col|
+        yield row, col
+      end
+    end
+  end
+
+  def each_adjacent_cell(matrix, row, col)
+    maxCol = matrix[row].length - 1
+    maxRow = matrix.length - 1
+
+    yield row - 1, col - 1 if row > 0 && col > 0
+    yield row - 1, col     if row > 0
+    yield row - 1, col + 1 if row > 0 && col < maxCol
+    yield row, col - 1     if col > 0
+    yield row, col + 1     if col < maxCol
+    yield row + 1, col - 1 if row < maxRow && col > 0
+    yield row + 1, col     if row < maxRow
+    yield row + 1, col + 1 if row < maxRow && col < maxCol
+  end
+end
